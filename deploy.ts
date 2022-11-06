02b80f4a0332e2a3eccd789ffee6573a6810acde
akash <dhanviakash1234@gmail.com>
2022-11-07 02:12:30 +0530

    typescript code added

--- deploy.ts
+++ deploy.ts
@@ -1,16 +1,16 @@
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require("dotenv").config();
+import { ethers } from "ethers";
+import * as fs from "fs-extra";
+import "dotenv/config";
 
 async function main() {
   const provider = await new ethers.providers.JsonRpcProvider(
     process.env.RPC_URL
   );
-  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-  const key = fs.readFileSync("./.encryptedKey.json");
-  let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+  const key = fs.readFileSync("./.encryptedKey.json", "utf-8");
+  let wallet = ethers.Wallet.fromEncryptedJsonSync(
     key,
-    process.env.PRIVATE_KEY_PASSWORD
+    process.env.PRIVATE_KEY_PASSWORD!
   );
   wallet = wallet.connect(provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
